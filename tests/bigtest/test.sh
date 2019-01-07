87c44bcae20e2adb86e095fad603ef68dc9289ba
Kerby Shedden <kshedden@umich.edu>
2019-01-06 21:29:02 -0500

    update

--- tests/bigtest/test.sh
+++ tests/bigtest/test.sh
@@ -3,11 +3,12 @@
 TARGET=/var/tmp/muscato_test
 mkdir -p $TARGET
 
-muscato_gendat -NumRead=10000000 -NumGene=10000000 -Dir=${TARGET}
+muscato_gendat -NumRead=100000 -NumGene=100000 -Dir=${TARGET}
 
-muscato_prep_targets ${TARGET}/genes.txt.gz
+echo "Starting prep_targets"
+muscato_prep_targets ${TARGET}/genes.txt.sz
 
-muscato -GeneIdFileName=${TARGET}/genes_ids.txt.sz -GeneFileName=${TARGET}/genes.txt.sz \
+muscato -GeneIdFileName=${TARGET}/musc_ids_genes.txt.sz -GeneFileName=${TARGET}/musc_genes.txt.sz \
         -ReadFileName=${TARGET}/reads.fastq -WindowWidth=20 -Windows=10,30,50,70 -MaxReadLength=200 \
         -TempDir=${TARGET}/muscato_tmp -NoCleanTemp -ResultsFileName=${TARGET}/results.txt
 
